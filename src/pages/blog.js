3e65e17bc03ce853110673555f7e700c82306c58
Yoni <yoniendyoni@gmail.com>
2021-10-21 22:16:23 +0300

    lalala

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,13 +1,42 @@
 import React from "react";
 import Layout from "../components/layout";
-
+import { Link,graphql,useStaticQuery } from "gatsby";
 
 
 const BlogPage = () => {
+    const data = useStaticQuery(graphql`
+    query{
+        allMarkdownRemark{
+          edges{
+            node{
+              fields{
+                  slug
+              }
+              frontmatter{
+                title
+                date
+              }
+              html
+              excerpt
+            }
+          }
+        }
+      }
+    `)
     return (
         <Layout>
             <h1>Blog</h1>
-            <p>Posts will show up here later on.</p>
+            <ol>
+            {data.allMarkdownRemark.edges.map(edge =>{
+                return <li>
+                    <Link to={`/blog/${edge.node.fields.slug}`}>
+                    <h2>{edge.node.frontmatter.title}</h2>
+                    <p>{edge.node.frontmatter.date}</p>
+                    </Link>
+                    </li>
+            }
+            )}
+            </ol>
         </Layout>
       )
 }
